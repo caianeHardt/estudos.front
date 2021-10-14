55b64c6e5ef50d1dddc06a2a778717010ce56c75
Caiane Hardt <caianehardt@gmail.com>
2021-10-14 18:10:28 -0300

    add material dos cursos de front - html e css

---
+++ alura/javascript/js/calcula-imc.js
@@ -0,0 +1,46 @@
+var titulo = document.querySelector(".titulo");  
+titulo.textContent = "Aparecida Nutricionista"; 
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = calculaImc(peso,altura);    
+        tdImc.textContent = imc;
+
+    }
+}
+
+function calculaImc(peso,altura) {
+       imc = peso / ( altura * altura);
+
+        return imc.toFixed(2);
+}   
